d5484495d3512742e73918609babd56994b1f901
raveeshsundaram77-code <raveeshsundaram77@gmail.com>
2026-06-12 14:58:05 +0530

    added operators.js

--- operators.js
+++ operators.js
@@ -1,4 +1,4 @@
-//Arithmatic operators 
+console.log("arithmetic operators" ) 
 var a =10
 var b=20
 console.log(a+b)
@@ -31,3 +31,15 @@ console.log(a>0 && b>0)
 console.log(a<0 && b>0)
 console.log(a>0 || b<0)
 console.log(!(a>0))
+
+console.log("unary oparetors")
+var c=30
+console.log(c++)
+console.log(c--)
+console.log(++c)
+console.log(--c)
+
+console.log("ternary oparetors")
+var result = (a>b) ? "a is greater " : "b is greater";
+console.log(result)
+
