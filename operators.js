a492bf0c80d4f0f3578b018690c9923fced16d8d
raveeshsundaram77-code <raveeshsundaram77@gmail.com>
2026-06-12 12:09:37 +0530

    added operators.js

--- operators.js
+++ operators.js
@@ -7,3 +7,27 @@ console.log(a*b)
 console.log(a/b)
 console.log(a%b)
 console.log(a**b)
+
+
+console.log("assignment operators")
+var c=25
+console.log(c+=50)
+console.log(c-=50) 
+console.log(c*=50)
+console.log(c/=50)
+console.log(c%=50)
+
+console.log("comparision operators")
+
+console.log(a==b)
+console.log(a!=b)
+console.log(a<=b)
+console.log(a>=b)
+console.log(a<b)
+console.log(a>b)
+
+console.log("logical operators")
+console.log(a>0 && b>0)
+console.log(a<0 && b>0)
+console.log(a>0 || b<0)
+console.log(!(a>0))
