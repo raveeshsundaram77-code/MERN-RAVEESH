13a1538b89ad836b9da33b43df44807e3a603d23
raveeshsundaram77-code <raveeshsundaram77@gmail.com>
2026-06-13 13:50:02 +0530

    added function.j

---
+++ conditional.js
@@ -0,0 +1,5 @@
+// printing 2 tables 
+for (let i=1;i<=10;i++){
+    console.log(i + " x 2 =" + i*2);
+    
+}
---
+++ functions.js
@@ -0,0 +1,24 @@
+// Non parametric function
+function great(){
+    console.log("Hello world");
+}
+great()
+
+
+// parametric funtions 
+function add(a,b){
+    return a+b
+}
+console.log(add(5,3));
+
+
+
+function fav(factor,fplayer,fmovie){
+    console.log("Fav actor:" + factor);
+    console.log("Fav player:" + fplayer);
+    console.log("Fav movie :" + fmovie);
+    
+    
+}
+fav("seeman","Virat","mersal")
+
