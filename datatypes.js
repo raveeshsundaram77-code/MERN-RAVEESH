19cae87b8456432a961c0bd6d04cdacf96c694b8
raveeshsundaram77-code <raveeshsundaram77@gmail.com>
2026-06-15 11:18:32 +0530

    added datatype.js

--- datatypes.js
+++ datatypes.js
@@ -1,1 +1,50 @@
-console.log("premitive")
+console.log("welcome to primitive datatypes");
+
+//number
+console.log("datatype number");
+var age = 30;
+console.log(age);
+
+//string
+console.log("datatypes string");
+ var name = "joy";
+ console.log(name);
+
+//boolean
+ console.log("data types boolean");
+ var boolean = "true";
+ console.log(boolean);
+
+//BigInt
+ console.log("data types bigInt");
+ var big = 133324455678n;
+ console.log(big);
+
+//null
+ console.log("data types null");
+ var isstudent ="true";
+ console.log(isstudent);
+
+//symbols
+ console.log("data types symbols");
+ var symbolsvalue = Symbol('symbols');
+ console.log(symbolsvalue);
+
+ //undefined
+    console.log("data types undefined");
+    var undefined ;
+    console.log(undefined )
+
+    //non-primitive
+    console.log("objects");
+    var person={
+        name:"help",
+        age:5
+    };
+    console.log(person.age);
+
+    //Array
+    console.log("Array");
+    var number =["kiwi","apple","pine apple","grapes","fig"];
+    console.log(number.length);
+    console.log(number[3]);
