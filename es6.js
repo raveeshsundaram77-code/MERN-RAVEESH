a95fc11baf4058a9f0cc26b1e4788355cdeef094
raveeshsundaram77-code <raveeshsundaram77@gmail.com>
2026-06-15 13:50:30 +0530

    added es6.js

---
+++ es6.js
@@ -0,0 +1,35 @@
+// es6 examples 
+
+
+// let 
+let student=10
+student=11
+console.log(student);
+
+//const
+const CollageName="VETIAS"
+console.log(CollageName);
+
+// Tamplate literals
+let studentName="Arun"
+console.log(`welcome ${studentName} to ${CollageName}`)
+
+//object
+
+const student={name:"Arun",age:20,department:"IT"}
+console.log(student);
+
+// Destructure
+
+const {name,age,department}=student
+console.log("Name:",name);
+console.log("Age:",age);
+console.log("Department:",department);
+
+//Arrow fuction
+const displayfunction=()=>{
+    console.log("Student Name:")
+}
+
+
+
