a33b953428b230b0b0ed1059b00f20854da9da54
raveeshsundaram77-code <raveeshsundaram77@gmail.com>
2026-06-15 11:28:32 +0530

    added conditional.js

--- conditional.js
+++ conditional.js
@@ -1,13 +1,45 @@
-// printing 2 tables 
+// for loop 
+console.log("for loop");
+
 for (let i=1;i<=10;i++){
     console.log(i + " x 2 =" + i*2);
     
 } 
 
+// while loop
 
-console.log(" // while loop ")
+console.log("while loop ")
 a=0
 while (a<=10){
     console.log(a)
     a++
+}
+//do while loop
+
+console.log("do while loop");
+
+let count=1;
+do{
+    console.log(count);
+    count++;
+}
+while(count<=10)
+    console.log("end of program");
+
+//switch case 
+console.log("switch case");
+
+var light= "white";
+switch(light){
+    case "green":
+        console.log("go");
+        break;
+    case "yellow":
+        console.log("slow down");
+        break;
+    case "red":
+        console.log("stop");
+        break;
+    default:
+        console.log("invalid light");
 }
